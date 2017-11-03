db8362e5c53068b9c8f6705d9b7bf988004399c1
Rafael Furry <rfurry@gmail.com>
2017-11-03 08:22:23 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    AdminComponent,
+    PlayersComponent,
+    PlayerDetailComponent,
+    EditPlayerComponent
   ],
   imports: [
     BrowserModule
